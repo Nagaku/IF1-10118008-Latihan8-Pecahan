357dc96958d8841320996b3a2b01dc66ad40c6a4
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-09-26 18:39:40 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan8-Pecahan/src/latihan8/Latihan8.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan8;
+
+/**
+ *
+ *  @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF1
+ * NIM			: 10118008
+ * Deskripsi Program	: Program ini berisi program untuk menampilkan biodata rockstar
+ */
+public class Latihan8 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		float f;
+		double d;
+		f = 123456789.123456789F;
+		d = 1_234_567_890.123456789;
+		System.out.println("Float : " + f);	
+		System.out.println("Double : " + d);
+	}
+	
+}
